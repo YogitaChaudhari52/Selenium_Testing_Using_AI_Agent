1cab99ab7d9e5544ef6e54d29edac28367bef141
cyogi <jobforyogita358@gmail.com>
2025-09-06 16:59:30 +0530

    Updated code with extent report and also loggerre

--- src/test/java/StepDefination/AddUserStepDef.java
+++ src/test/java/StepDefination/AddUserStepDef.java
@@ -5,6 +5,7 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.openqa.selenium.By;
+import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
